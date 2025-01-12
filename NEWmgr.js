a06c9c7fce7a71340719647afd170f962d4c861b
Psychology of Pragmatics Lab <poplab.scsd@gmail.com>
2025-01-12 21:05:12 +0100

    Update NEWmgr.js

--- NEWmgr.js
+++ NEWmgr.js
@@ -1,18 +1,11 @@
-define(['managerAPI',
-		'https://cdn.jsdelivr.net/gh/minnojs/minno-datapipe@1.*/datapipe.min.js'], function(Manager){
-
-
-	//You can use the commented-out code to get parameters from the URL.
-	//const queryString = window.location.search;
-    //const urlParams = new URLSearchParams(queryString);
-    //const pt = urlParams.get('pt');
-
-	var API    = new Manager();
-	//const subid = Date.now().toString(16)+Math.floor(Math.random()*10000).toString(16);
-	init_data_pipe(API, 'zpjKaPesdEOI',  {file_type:'csv'});	
+define(['managerAPI', 'https://cdn.jsdelivr.net/gh/minnojs/minno-datapipe@1.*/datapipe.min.js'], function(Manager) {
+    let API = new Manager();
 
     API.setName('mgr');
     API.addSettings('skip',true);
+	
+	//IMPORTANT: Replace the 2nd argument with your DataPipe Experiment ID
+	init_data_pipe(API, 'o3fzbHp75HLot5hIbZMCYS3siiAyLxE8yJkNQsqT6wYD4jPq90qvPtmooF98mRCTltjirG',  {file_type:'csv'});	
 
     //Randomly select which of two sets of category labels to use.
     let raceSet = API.shuffle(['a','b'])[0];
@@ -64,6 +57,12 @@ define(['managerAPI',
             buttonText: 'Continue'
         }],
 
+        prolificid: [{
+            type: 'quest',
+            name: 'prolificid',
+            scriptUrl: 'prolificid.js'
+        }],
+
         intro: [{
             inherit: 'instructions',
             name: 'intro',
@@ -102,57 +101,23 @@ define(['managerAPI',
             header: 'You have completed the study'
         }], 
         
+   		//This task waits until the data are sent to the server.
+        uploading: uploading_task({header: 'just a moment', body:'Please wait, sending data... '}),
+		
         //Use if you want to redirect the participants elsewhere at the end of the study
         redirect:
         [{ 
-			//Replace with any URL you need to put at the end of your study, or just remove this task from the sequence below
-            type:'redirect', name:'redirecting', url: 'https://www.google.com/search' 
-        }],
-		
-		//This task waits until the data are sent to the server.
-        uploading: uploading_task({header: 'just a moment', body:'Please wait, sending data... '})
+			//At the end of the url, insert your Prolific study code
+            type:'redirect', name:'redirecting', url: 'https://app.prolific.co/submissions/complete?cc=YOURPROLIFICCODE' 
+        }]
+
     });
 
     API.addSequence([
-        { type: 'isTouch' }, //Use Minno's internal touch detection mechanism. 
         
-        { type: 'post', path: ['$isTouch', 'raceSet', 'blackLabels', 'whiteLabels'] },
+        { type: 'post', path: ['raceSet', 'blackLabels', 'whiteLabels'] },
 
-        // apply touch only styles
-        {
-            mixer:'branch',
-            conditions: {compare:'global.$isTouch', to: true},
-            data: [
-                {
-                    type: 'injectStyle',
-                    css: [
-                        '* {color:red}',
-                        '[piq-page] {background-color: #fff; border: 1px solid transparent; border-radius: 4px; box-shadow: 0 1px 1px rgba(0, 0, 0, 0.05); margin-bottom: 20px; border-color: #bce8f1;}',
-                        '[piq-page] > ol {margin: 15px;}',
-                        '[piq-page] > .btn-group {margin: 0px 15px 15px 15px;}',
-                        '.container {padding:5px;}',
-                        '[pi-quest]::before, [pi-quest]::after {content: " ";display: table;}',
-                        '[pi-quest]::after {clear: both;}',
-                        '[pi-quest] h3 { border-bottom: 1px solid transparent; border-top-left-radius: 3px; border-top-right-radius: 3px; padding: 10px 15px; color: inherit; font-size: 2em; margin-bottom: 20px; margin-top: 0;background-color: #d9edf7;border-color: #bce8f1;color: #31708f;}',
-                        '[pi-quest] .form-group > label {font-size:1.2em; font-weight:normal;}',
-
-                        '[pi-quest] .btn-toolbar {margin:15px;float:none !important; text-align:center;position:relative;}',
-                        '[pi-quest] [ng-click="decline($event)"] {position:absolute;right:0;bottom:0}',
-                        '[pi-quest] [ng-click="submit()"] {width:30%;line-height: 1.3333333;border-radius: 6px;}',
-                        // larger screens
-                        '@media (min-width: 480px) {',
-                        ' [pi-quest] [ng-click="submit()"] {width:30%;padding: 10px 16px;font-size: 1.6em;}',
-                        '}',
-                        // phones and smaller screens
-                        '@media (max-width: 480px) {',
-                        ' [pi-quest] [ng-click="submit()"] {padding: 8px 13px;font-size: 1.2em;}',
-                        ' [pi-quest] [ng-click="decline($event)"] {font-size: 0.9em;padding:3px 6px;}',
-                        '}'
-                    ]
-                }
-            ]
-        },
-        
+        {inherit: 'prolificid'},
         
         {inherit: 'intro'},
         {
@@ -160,7 +125,6 @@ define(['managerAPI',
             data:[
                 {inherit: 'explicits'},
 
-                // force the instructions to preceed the iat
                 {
                     mixer: 'wrapper',
                     data: [
@@ -171,7 +135,7 @@ define(['managerAPI',
             ]
         },
 
-		{inherit: 'uploading'},
+        {inherit: 'uploading'},
         {inherit: 'lastpage'},
         {inherit: 'redirect'}
     ]);
