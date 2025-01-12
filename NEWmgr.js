b3c449391d835d2071a6557e6984d9f7c3065148
Psychology of Pragmatics Lab <poplab.scsd@gmail.com>
2025-01-12 20:43:01 +0100

    Update NEWmgr.js

--- NEWmgr.js
+++ NEWmgr.js
@@ -1,113 +1,179 @@
-define(['managerAPI', 
-    'https://cdn.jsdelivr.net/gh/minnojs/minno-datapipe@1.*/datapipe.min.js'], 
-function(Manager) {
+define(['managerAPI',
+		'https://cdn.jsdelivr.net/gh/minnojs/minno-datapipe@1.*/datapipe.min.js'], function(Manager){
 
-    // Initialize Manager and data pipe
-    var API = new Manager();
-    init_data_pipe(API, 'your-study-id', {file_type: 'csv'}); // Replace 'your-study-id' with your actual study ID
 
-    API.setName('sciat');
-    API.addSettings('skip', true);
+	//You can use the commented-out code to get parameters from the URL.
+	//const queryString = window.location.search;
+    //const urlParams = new URLSearchParams(queryString);
+    //const pt = urlParams.get('pt');
 
-    // Define stimuli
-    const targetCategory = 'Autistics';
-    const posWords = ['Love', 'Joy', 'Pleasure', 'Peace'];
-    const negWords = ['Terrible', 'Angry', 'Hate', 'Evil'];
-    const autisticsWords = ['Repeats body movements', 'Avoids eye contact', 'Dislikes unexpected changes', 'Struggles making friends', 'Repeats certain words'];
+	var API    = new Manager();
+	//const subid = Date.now().toString(16)+Math.floor(Math.random()*10000).toString(16);
+	init_data_pipe(API, 'zpjKaPesdEOI',  {file_type:'csv'});	
 
-    // Add global variables
+    API.setName('mgr');
+    API.addSettings('skip',true);
+
+    //Randomly select which of two sets of category labels to use.
+    let raceSet = API.shuffle(['a','b'])[0];
+    let blackLabels = [];
+    let whiteLabels = [];
+
+    if (raceSet == 'a') {
+        blackLabels.push('African Americans');
+        whiteLabels.push('European Americans');
+    } else {
+        blackLabels.push('Black people');
+        whiteLabels.push('White people');
+    }
+
     API.addGlobal({
-        baseURL: './images/', // Retained for potential future use
-        targetCategory: targetCategory,
-        posWords: API.shuffle(posWords),
-        negWords: API.shuffle(negWords),
-        autisticsWords: API.shuffle(autisticsWords)
+        raceiat:{},
+        //YBYB: change when copying back to the correct folder
+        baseURL: './images/',
+        raceSet:raceSet,
+        blackLabels:blackLabels,
+        whiteLabels:whiteLabels,
+        //Select randomly what attribute words to see. 
+        //Based on Axt, Feng, & Bar-Anan (2021).
+        posWords : API.shuffle([
+            'Love', 'Cheer', 'Friend', 'Pleasure',
+            'Adore', 'Cheerful', 'Friendship', 'Joyful', 
+            'Smiling','Cherish', 'Excellent', 'Glad', 
+            'Joyous', 'Spectacular', 'Appealing', 'Delight', 
+            'Excitement', 'Laughing', 'Attractive','Delightful', 
+            'Fabulous', 'Glorious', 'Pleasing', 'Beautiful', 
+            'Fantastic', 'Happy', 'Lovely', 'Terrific', 
+            'Celebrate', 'Enjoy', 'Magnificent', 'Triumph'
+        ]), 
+        negWords : API.shuffle([
+            'Abuse', 'Grief', 'Poison', 'Sadness', 
+            'Pain', 'Despise', 'Failure', 'Nasty', 
+            'Angry', 'Detest', 'Horrible', 'Negative', 
+            'Ugly', 'Dirty', 'Gross', 'Evil', 
+            'Rotten','Annoy', 'Disaster', 'Horrific',  
+            'Scorn', 'Awful', 'Disgust', 'Hate', 
+            'Humiliate', 'Selfish', 'Tragic', 'Bothersome', 
+            'Hatred', 'Hurtful', 'Sickening', 'Yucky'
+        ])
     });
 
-    // Define SC-IAT tasks
     API.addTasksSet({
         instructions: [{
             type: 'message',
             buttonText: 'Continue'
         }],
+
         intro: [{
             inherit: 'instructions',
             name: 'intro',
             templateUrl: 'intro.jst',
-            title: 'Welcome',
-            header: 'Welcome to the SC-IAT'
+            title: 'Intro',
+            header: 'Welcome'
         }],
-        sciat_instructions: [{
+
+        raceiat_instructions: [{
             inherit: 'instructions',
-            name: 'sciat_instructions',
-            templateUrl: 'sciat_instructions.jst',
-            title: 'SC-IAT Instructions',
-            header: 'Single Category IAT'
+            name: 'raceiat_instructions',
+            templateUrl: 'raceiat_instructions.jst',
+            title: 'IAT Instructions',
+            header: 'Implicit Association Test'
         }],
-        sciat: [{
+
+        explicits: [{
+            type: 'quest',
+            name: 'explicits',
+            scriptUrl: 'explicits.js'
+        }],
+
+        raceiat: [{
             type: 'time',
-            name: 'sciat',
-            script: function() {
-                return {
-                    blocks: [
-                        // Practice Block
-                        {
-                            name: 'Block 1 - Practice',
-                            type: 'iat',
-                            layout: {
-                                left1: 'Positive',
-                                right1: 'Negative',
-                                right2: targetCategory
-                            },
-                            stimuli: [
-                                {type: 'target', data: {category: 'Positive', words: posWords}},
-                                {type: 'target', data: {category: 'Negative', words: negWords}},
-                                {type: 'target', data: {category: targetCategory, words: autisticsWords}}
-                            ],
-                            trialsPerCategory: 20
-                        },
-                        // Test Block
-                        {
-                            name: 'Block 2 - Test',
-                            type: 'iat',
-                            layout: {
-                                left1: targetCategory,
-                                right1: 'Positive',
-                                right2: 'Negative'
-                            },
-                            stimuli: [
-                                {type: 'target', data: {category: targetCategory, words: autisticsWords}},
-                                {type: 'target', data: {category: 'Positive', words: posWords}},
-                                {type: 'target', data: {category: 'Negative', words: negWords}}
-                            ],
-                            trialsPerCategory: 40
-                        }
-                    ]
-                };
-            }()
+            name: 'raceiat',
+            scriptUrl: 'raceiat.js'
         }],
+
         lastpage: [{
             type: 'message',
             name: 'lastpage',
             templateUrl: 'lastpage.jst',
             title: 'End',
-            header: 'Thank you for participating'
+            //Uncomment the following if you want to end the study here.
+            //last:true, 
+            header: 'You have completed the study'
+        }], 
+        
+        //Use if you want to redirect the participants elsewhere at the end of the study
+        redirect:
+        [{ 
+			//Replace with any URL you need to put at the end of your study, or just remove this task from the sequence below
+            type:'redirect', name:'redirecting', url: 'https://www.google.com/search' 
         }],
-        redirect: [{ 
-            type: 'redirect', 
-            name: 'redirecting', 
-            url: 'https://www.yourcompletionpage.com' // Replace with your actual redirect URL
-        }]
+		
+		//This task waits until the data are sent to the server.
+        uploading: uploading_task({header: 'just a moment', body:'Please wait, sending data... '})
     });
 
-    // Define study sequence
     API.addSequence([
-        { type: 'isTouch' }, 
-        { inherit: 'intro' },
-        { inherit: 'sciat_instructions' },
-        { inherit: 'sciat' },
-        { inherit: 'lastpage' },
-        { inherit: 'redirect' }
+        { type: 'isTouch' }, //Use Minno's internal touch detection mechanism. 
+        
+        { type: 'post', path: ['$isTouch', 'raceSet', 'blackLabels', 'whiteLabels'] },
+
+        // apply touch only styles
+        {
+            mixer:'branch',
+            conditions: {compare:'global.$isTouch', to: true},
+            data: [
+                {
+                    type: 'injectStyle',
+                    css: [
+                        '* {color:red}',
+                        '[piq-page] {background-color: #fff; border: 1px solid transparent; border-radius: 4px; box-shadow: 0 1px 1px rgba(0, 0, 0, 0.05); margin-bottom: 20px; border-color: #bce8f1;}',
+                        '[piq-page] > ol {margin: 15px;}',
+                        '[piq-page] > .btn-group {margin: 0px 15px 15px 15px;}',
+                        '.container {padding:5px;}',
+                        '[pi-quest]::before, [pi-quest]::after {content: " ";display: table;}',
+                        '[pi-quest]::after {clear: both;}',
+                        '[pi-quest] h3 { border-bottom: 1px solid transparent; border-top-left-radius: 3px; border-top-right-radius: 3px; padding: 10px 15px; color: inherit; font-size: 2em; margin-bottom: 20px; margin-top: 0;background-color: #d9edf7;border-color: #bce8f1;color: #31708f;}',
+                        '[pi-quest] .form-group > label {font-size:1.2em; font-weight:normal;}',
+
+                        '[pi-quest] .btn-toolbar {margin:15px;float:none !important; text-align:center;position:relative;}',
+                        '[pi-quest] [ng-click="decline($event)"] {position:absolute;right:0;bottom:0}',
+                        '[pi-quest] [ng-click="submit()"] {width:30%;line-height: 1.3333333;border-radius: 6px;}',
+                        // larger screens
+                        '@media (min-width: 480px) {',
+                        ' [pi-quest] [ng-click="submit()"] {width:30%;padding: 10px 16px;font-size: 1.6em;}',
+                        '}',
+                        // phones and smaller screens
+                        '@media (max-width: 480px) {',
+                        ' [pi-quest] [ng-click="submit()"] {padding: 8px 13px;font-size: 1.2em;}',
+                        ' [pi-quest] [ng-click="decline($event)"] {font-size: 0.9em;padding:3px 6px;}',
+                        '}'
+                    ]
+                }
+            ]
+        },
+        
+        
+        {inherit: 'intro'},
+        {
+            mixer:'random',
+            data:[
+                {inherit: 'explicits'},
+
+                // force the instructions to preceed the iat
+                {
+                    mixer: 'wrapper',
+                    data: [
+                        {inherit: 'raceiat_instructions'},
+                        {inherit: 'raceiat'}
+                    ]
+                }
+            ]
+        },
+
+		{inherit: 'uploading'},
+        {inherit: 'lastpage'},
+        {inherit: 'redirect'}
     ]);
 
     return API.script;
