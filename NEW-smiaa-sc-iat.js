885fda0a034be31952082d7b3132a7a92c0fffb0
Psychology of Pragmatics Lab <poplab.scsd@gmail.com>
2025-01-13 18:46:45 +0100

    Update NEW-smiaa-sc-iat.js

--- NEW-smiaa-sc-iat.js
+++ NEW-smiaa-sc-iat.js
@@ -1,5 +1,3 @@
-<script src="https://cdnjs.cloudflare.com/ajax/libs/require.js/2.3.6/require.min.js"></script>
-
 <script>
     // Ensure all the dependencies are loaded properly using require.js
     require(['https://cdn.jsdelivr.net/gh/baranan/minno-tasks@0.*/stiat/qualtrics/qstiat6.js'], function(stiatExtension) {
